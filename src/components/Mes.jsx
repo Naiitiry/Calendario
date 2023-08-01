5deaeb0f846040a9a549c82595f2c7be5a1c307b
Naiitiry <danchukroman@gmail.com>
2023-08-01 17:53:41 -0300

    "Mejoras al calendario y css"

--- src/components/Mes.jsx
+++ src/components/Mes.jsx
@@ -3,12 +3,16 @@ import Dias from "./Dia.jsx"
 
 const Meses = ({monthName})=>{
     const num = Array.from({length:31},(_,index)=>index+1);
+    const week = ['D', 'L', 'M', 'Mi', 'J', 'V', 'S'];
 
     return(
         <>
             <div className='mes'>
                 <h2>{monthName}</h2>
                 <div className="dias-container">
+                    {week.map((week)=>(
+                        <h3 key={week}>{week}</h3>
+                    ))}
                     {num.map((num,index)=>(
                         <Dias key={`num-${index}`} value={num}/>
                     ))}
